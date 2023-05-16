48615da5d9343e4feed6c3c33e00ceddbb50f5f5
Hermes <akochenokvm@gmail.com>
2023-05-17 01:14:29 +0300

    2. project: counter(add some styles)

--- src/counter.js
+++ src/counter.js
@@ -5,14 +5,24 @@ class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            color: 'blue'
         }
     }
     increment = () => {
-        this.setState({count: this.state.count + 1})
+        this.setState({count: this.state.count + 1}, this.updateColor)
     }
     decrement = () => {
-        this.setState({count: this.state.count - 1})
+        this.setState({count: this.state.count - 1},  this.updateColor)
+    }
+    updateColor = () => {
+        if(this.state.count > 5){
+            this.setState({color: 'green'})
+        } else if(this.state.count < -5){
+            this.setState({color: 'red'})
+        }else{
+            this.setState({color: 'blue'})
+        }
     }
     render() {
         return (
@@ -21,7 +31,7 @@ class Counter extends Component {
                     <button onClick={this.increment}>+</button>
                     <button onClick={this.decrement}>-</button>
                 </div>
-              <p className="counter-value">Counter: {this.state.count}</p>
+              <p className="counter-value">Counter: <span style={{color: this.state.color}}>{this.state.count}</span></p>
             </div>
         );
     }
